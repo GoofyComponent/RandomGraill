3a1236ae86a5d6c5ccb6724261ac9ea2de31c376
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2024-09-09 16:16:34 +0200

    ci: add commitlint & husky

---
+++ commitlint.config.ts
@@ -0,0 +1,25 @@
+import { RuleConfigCondition, RuleConfigSeverity } from "@commitlint/types";
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "wip",
+      ],
+    ] as [RuleConfigSeverity, RuleConfigCondition, string[]],
+  },
+};
