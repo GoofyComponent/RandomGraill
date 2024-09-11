ef89764f439994177d4cb82d637e9fa7e2461eb6
lucag322 <97728486+lucag322@users.noreply.github.com>
2024-09-11 14:10:31 +0200

    style : correction sur la size des cards

--- front/src/pages/Example.tsx
+++ front/src/pages/Example.tsx
@@ -1,5 +1,4 @@
 import CardResto from '@/components/project/cardResto';
-import CardRoulette from '@/components/project/cardRoulette';
 import { Button } from '@/components/ui/button';
 
 export const Example = () => {
@@ -7,26 +6,21 @@ export const Example = () => {
     <div>
       <p>Example</p>
       <Button>Ceci est un button shadcn</Button>
-      <CardResto
-        bgImage="https://www.mcdo-strasbourg.fr/wp-content/uploads/2022/03/IMG_20220609_09374722-scaled-e1655131454477.jpg"
-        name="Mcdo"
-        distance="500m"
-        url="/restaurant/le-petit-bistro"
-        variant="default"
-        clickable={true}
-      />
       <div className="flex">
-        <CardRoulette
-          name="Roulette Hetic"
+        <CardResto
+          bgImage="https://www.mcdo-strasbourg.fr/wp-content/uploads/2022/03/IMG_20220609_09374722-scaled-e1655131454477.jpg"
+          name="Mcdo"
+          distance="500m"
           url="/restaurant/le-petit-bistro"
           variant="default"
           clickable={true}
         />
-        <CardRoulette
-          name="Ajouter une roulette"
-          url="/add-roulette"
+        <CardResto
+          bgImage="https://www.mcdo-strasbourg.fr/wp-content/uploads/2022/03/IMG_20220609_09374722-scaled-e1655131454477.jpg"
+          name="Mcdo"
+          distance="500m"
+          url="/restaurant/le-petit-bistro"
           variant="default"
-          usage="add"
           clickable={true}
         />
       </div>
