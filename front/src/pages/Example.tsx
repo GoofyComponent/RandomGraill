eebcaeb53bc806edaa254578703a32ede42f0405
lucag322 <97728486+lucag322@users.noreply.github.com>
2024-09-11 10:18:57 +0200

    feat : Card restaurants
    ajout de la card resto qui comprend les props suivant :
    
      bgImage: string;
      name: string;
      distance: string;
      url: string;
      headerBgColor?: string;
      footerBgColor?: string;
      variant?: 'default' | 'carousel' | 'disabled' | 'selected';
      clickable?: boolean;
    
    :+1:

--- front/src/pages/Example.tsx
+++ front/src/pages/Example.tsx
@@ -1,3 +1,6 @@
+import { f } from '@vite-pwa/assets-generator/dist/shared/assets-generator.5e51fd40.mjs';
+
+import CardResto from '@/components/project/cardResto';
 import { Button } from '@/components/ui/button';
 
 export const Example = () => {
@@ -5,6 +8,14 @@ export const Example = () => {
     <div>
       <p>Example</p>
       <Button>Ceci est un button shadcn</Button>
+      <CardResto
+        bgImage="https://www.mcdo-strasbourg.fr/wp-content/uploads/2022/03/IMG_20220609_09374722-scaled-e1655131454477.jpg"
+        name="Le Petit Bistro"
+        distance="500m"
+        url="/restaurant/le-petit-bistro"
+        variant="default"
+        clickable={true}
+      />
     </div>
   );
 };
